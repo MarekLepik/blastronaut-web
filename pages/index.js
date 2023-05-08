1d014691be674b2b6916ec54962173fb5f743683
mareklepik <marek.lepik@outlook.com>
2023-05-08 19:27:38 +0300

    minor fixes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import Details from "../components/Details";
 import Hero from "../components/Hero";
 import CTA2 from "../components/CTA2";
